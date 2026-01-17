e6ec92f1bdd6b04901b9be14c371b3536c9319be
polar-sucks-at-coding <melihekinci@protonmail.me>
2026-01-17 15:15:37 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- main.cpp
+++ main.cpp
@@ -28,7 +28,8 @@ namespace undistract
         if (added) {final_message = "Added " + user.username + " to the list.";}
 
         else {final_message = "Removed " + user.username + " from the list.";}
-        event.from()->creator->message_create(dpp::message(event.command.channel_id, final_message));
+        event.reply(final_message);
+        //event.from()->creator->message_create(dpp::message(event.command.channel_id, final_message));
     }
     void addToUndistractList(const dpp::user& user)
     {
@@ -92,7 +93,16 @@ void ragebait(const dpp::message_create_t& event)
     srand(time(0));
     uint random = rand() % 50;
 
-    if (random) {std::cout << "message not getting ragebaited" << std::endl; return;}
+    const long& current_channel_id = event.msg.channel_id;
+    bool do_ragebait = 0;
+    for (long poo : ragebaitable_channels)
+    {
+        if (current_channel_id == poo) do_ragebait = 1;
+    }
+
+    if (random != 1) do_ragebait = 0;
+
+    if (!do_ragebait) {std::cout << "message not getting ragebaited" << std::endl; return;}
 
     size_t pool_size = sizeof(ragebait_pool)/sizeof(ragebait_pool[0]);
 
@@ -107,10 +117,9 @@ void logMessage(const dpp::message& msg)
     time_t now = time(0);
     char* date_time = ctime(&now);
     std::cout << "\nMessage sent by: " << msg.author.username << "\n";
-    std::cout << "Sent at: " << date_time;
+    std::cout << "Sent at: " << date_time; // no \n cuz "date_time" already has a new line at its end for some reason
     std::cout << "Message content: " << msg.content << std::endl;  
 }
-
 
 int main() 
 {    
@@ -129,9 +138,6 @@ int main()
         std::string command_name = event.command.get_command_name();
         if (command_name == "undistractor") 
         {
-
-            
-            event.reply("Adding you to the undistractor list...");
             undistract::addToUndistractList(author);
             undistract::addingToUndistractorListNotice(author, event);
 
@@ -173,10 +179,15 @@ int main()
         srand(time(0));
         const dpp::message& msg = event.msg;
 
-        if (msg.author.id != BOT_ID) undistract::undistract(msg, event, bot);
         logMessage(msg);
         fuckGlados(msg, event);
-        if (msg.channel_id == GENERAL_CHANNEL_ID && msg.author.id != BOT_ID) ragebait(event);
+
+        if (msg.author.id == BOT_ID) return;
+        //functions that aren't supposed to interact with the bot's own messages
+
+        undistract::undistract(msg, event, bot);
+        ragebait(event); 
+
     });
 
     bot.start(dpp::st_wait);
